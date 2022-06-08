bc77842630433d4ceb4fcca5bae616f281c09501
MorgantiWeb <info@morgantiweb.com>
2022-06-08 16:47:32 +0200

    static media item list (modules 1 to 3)

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { MediaItemComponent } from './media-item/media-item.component';
+import { MediaItemListComponent } from './media-item-list/media-item-list.component';
+import { FavoreDirective } from './favorite.directive';
+import { CategoryListPipe } from './category-list.pipe';
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        FormsModule
+    ],
+    declarations: [
+        AppComponent,
+        MediaItemComponent,
+        MediaItemListComponent,
+        FavoreDirective,
+        CategoryListPipe
+    ],
+    bootstrap: [
+        AppComponent
+    ]
+})
+export class AppModule {}
