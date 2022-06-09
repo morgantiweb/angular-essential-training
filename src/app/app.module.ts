69d4e75ee95a861eedf0a0b699e2a162acd9684d
MorgantiWeb <info@morgantiweb.com>
2022-06-09 12:43:43 +0200

    Services and injection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http'
 import { AppComponent } from './app.component';
 import { MediaItemComponent } from './media-item/media-item.component';
 import { MediaItemListComponent } from './media-item-list/media-item-list.component';
 import { FavoreDirective } from './favorite.directive';
 import { CategoryListPipe } from './category-list.pipe';
 import { MediaItemFormComponent } from './media-item-form/media-item-form.component';
+import { lookupListToken, lookupLists } from './providers';
 
 @NgModule({
     imports: [
         BrowserModule,
-        ReactiveFormsModule
+        ReactiveFormsModule,
+        HttpClientModule
     ],
     declarations: [
         AppComponent,
@@ -20,6 +23,9 @@ import { MediaItemFormComponent } from './media-item-form/media-item-form.compon
         FavoreDirective,
         CategoryListPipe,
         MediaItemFormComponent
+    ],
+    providers: [
+        { provide: lookupListToken, useValue: lookupLists }
     ],
     bootstrap: [
         AppComponent
