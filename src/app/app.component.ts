cf0097e767dd5c13ca0c00ae3799865b1dcdad25
MorgantiWeb <info@morgantiweb.com>
2022-06-09 17:19:31 +0200

    Special CSS selectors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component } from "@angular/core";
+import { Component, ViewEncapsulation } from "@angular/core";
 
 @Component({
   selector: "mw-app",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"],
+  encapsulation: ViewEncapsulation.Emulated
 })
 export class AppComponent {
 
